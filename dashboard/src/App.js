84d6851bc02b383327630fe5b01b00ad57136d76
Arild Andreassen <arildsa@hotmail.com>
2021-10-10 13:21:01 -0600

    --fully functional dashboard--

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,51 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Bar } from "react-chartjs-2";
-const ws = new WebSocket("ws://localhost:8082");
+import { io } from "socket.io-client";
+const socket = io();
 function App() {
-  const [data, setData] = useState(1);
+  const [graphData, setGraphdata] = useState([{ passed: 0, failed: 0 }]);
 
-  ws.addEventListener("message", ({ message }) => {
-    console.log(data);
-    // const newValue = data + 1;/
-    // console.log(newValue);
-    // setData(data + 1);
-  });
-
-  // ws.addEventListener("open", () => {
-  //   console.log("we are connected");
-  // });
-
-  // ws.addEventListener("message", ({ data }) => {
-  //   // console.log(data);
-  //   const jsonData = JSON.parse(data);
-  //   // console.log(data);
-  //   // console.log(graphs);
-  //   // console.log(graphs);
-  //   console.log(graphs);
-  //   // console.log(jsonData);
-  //   const passed = graphs[0].passed;
-  //   const newPassed = passed + 1;
-  //   const failed = graphs[0].failed + 1;
-  //   const newFailed = failed + 1;
-  //   console.log(graphs);
-  //   // setGraphs([{ passed: newPassed, failed: newFailed }]);
-
-  //   // setGraphs(jsonData);
-  // });
-
-  // const handleClick = () => {
-  //   fetch("/results", {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       console.log(response);
-  //     });
-  // };
+  useEffect(() => {
+    socket.on("update", (data) => {
+      console.log(data);
+      setGraphdata(data);
+    });
+  }, []);
 
   const state = {
     labels: ["Cypress"],
@@ -55,14 +21,14 @@ function App() {
         backgroundColor: "rgba(0, 177, 106, 1)",
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
-        data: [10],
+        data: [graphData[0].passed],
       },
       {
         label: "Failing",
         backgroundColor: "rgba(192, 57, 43, 1)",
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
-        data: [5],
+        data: [graphData[0].failed],
       },
     ],
   };
@@ -89,7 +55,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <button onClick={handleClick}></button> */}
+        <button>click me</button>
         <div>
           <Bar height={500} width={1000} data={state} options={options} />
         </div>
--- server.js
+++ server.js
@@ -1,54 +1,70 @@
 const express = require("express");
 const WebSocket = require("ws");
 const http = require("http");
+const socketIo = require("socket.io");
 const healthcheck = require("./routes/healthcheck");
 const results = require("./routes/results");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 const port = process.env.PORT || 5000;
-const wss = new WebSocket.Server({ port: 8082 });
+// const wss = new WebSocket.Server({ port: 8082 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", healthcheck);
 app.use("/", results);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`SERVER READY ON PORT ${port}`);
 });
 
-wss.on("connection", (ws) => {
-  console.log("new connection");
+io.on("connection", (socket) => {
+  console.log("connection");
+  const time = new Date();
+
+  const req = http.request(
+    {
+      hostname: "localhost",
+      port: 5000,
+      method: "GET",
+      path: "/results",
+    },
+    (result) => {
+      result.on("data", (data) => {
+        const json = JSON.parse(data);
+        console.log(`${time}: -updated results`);
+        socket.emit("update", json);
+      });
+    }
+  );
+  req.on("error", (error) => {
+    console.error(error);
+  });
+  req.end();
 
   setInterval(() => {
+    const time = new Date();
+
     const req = http.request(
       {
-        method: "GET",
         hostname: "localhost",
         port: 5000,
+        method: "GET",
         path: "/results",
       },
       (result) => {
         result.on("data", (data) => {
-          console.log("sending update");
           const json = JSON.parse(data);
-          ws.send(JSON.stringify(json));
+          console.log(`${time}: -updated results`);
+          io.emit("update", json);
         });
       }
     );
-
     req.on("error", (error) => {
-      console.log(error);
+      console.error(error);
     });
-
     req.end();
-  }, 5000);
-
-  ws.on("message", (data) => {
-    console.log("client has sent us " + data);
-  });
-
-  ws.on("close", () => {
-    console.log("closed");
-  });
+  }, 10000);
 });
